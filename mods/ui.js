d9bb272b43d1c1ef52a81aa04279aa3f3504e3d8
ThowZzy <thomasoshea112001@gmail.com>
2024-02-28 09:20:26 +0100

    Mods rewrite (allow no delay)

--- mods/ui.js
+++ mods/ui.js
@@ -2,54 +2,78 @@
 import './spatial-navigation-polyfill.js';
 import './ui.css';
 import { configRead, configWrite } from './config.js';
-import updateStyle from './theme.js';
 
-// We handle key events ourselves.
-window.__spatialNavigation__.keyMode = 'NONE';
-
-const ARROW_KEY_CODE = { 37: 'left', 38: 'up', 39: 'right', 40: 'down' };
-
-const uiContainer = document.createElement('div');
-uiContainer.classList.add('ytaf-ui-container');
-uiContainer.style['display'] = 'none';
-uiContainer.setAttribute('tabindex', 0);
-uiContainer.addEventListener(
-  'focus',
-  () => console.info('uiContainer focused!'),
-  true
-);
-uiContainer.addEventListener(
-  'blur',
-  () => console.info('uiContainer blured!'),
-  true
-);
-
-uiContainer.addEventListener(
-  'keydown',
-  (evt) => {
-    console.info('uiContainer key event:', evt.type, evt.keyCode);
-    if (evt.keyCode !== 404 && evt.keyCode !== 172) {
-      if (evt.keyCode in ARROW_KEY_CODE) {
-        navigate(ARROW_KEY_CODE[evt.keyCode]);
-      } else if (evt.keyCode === 13 || evt.keyCode === 32) {
-        // "OK" button
-        document.querySelector(':focus').click();
-        // Check if the focused element is a text input.
-      } else if (evt.keyCode === 27 && document.querySelector(':focus').type !== 'text') {
-        // Back button
-        uiContainer.style.display = 'none';
-        uiContainer.blur();
-      } else if (document.querySelector(':focus').type === 'text' && evt.keyCode === 27) {
-          const focusedElement = document.querySelector(':focus');
-          focusedElement.value = focusedElement.value.slice(0, -1);
+// Observer to catch when the video element is loaded, when it is -> execute the script
+const observer = new MutationObserver((mutationsList, observer) => {
+  mutationsList.forEach(mutation => {
+    mutation.addedNodes.forEach(addedNode => {
+      if (addedNode.tagName && addedNode.tagName.toLowerCase() === 'video') {
+        execute_once_dom_loaded();
+        observer.disconnect();
       }
-    }
-  },
-  true
-);
+    });
+  });
+});
 
-uiContainer.innerHTML = `
-<h1>TizenTube Configuration</h1>
+//Check if video element is already loaded. if not, wait for it once DOM is ready
+const videoElement = document.querySelector('video');
+if (videoElement) {
+  execute_once_dom_loaded();
+} else {
+  if (document.readyState !== 'loading') {
+    observer.observe(document.body, { childList: true, subtree: true });
+  } else {
+    document.addEventListener('DOMContentLoaded', () => {
+      observer.observe(document.body, { childList: true, subtree: true });
+    });
+  }
+}
+
+function execute_once_dom_loaded() {
+  // We handle key events ourselves.
+  window.__spatialNavigation__.keyMode = 'NONE';
+
+  var ARROW_KEY_CODE = { 37: 'left', 38: 'up', 39: 'right', 40: 'down' };
+
+  var uiContainer = document.createElement('div');
+  uiContainer.classList.add('ytaf-ui-container');
+  uiContainer.style['display'] = 'none';
+  uiContainer.setAttribute('tabindex', 0);
+  uiContainer.addEventListener(
+    'focus',
+    () => console.info('uiContainer focused!'),
+    true
+  );
+  uiContainer.addEventListener(
+    'blur',
+    () => console.info('uiContainer blured!'),
+    true
+  );
+
+  uiContainer.addEventListener(
+    'keydown',
+    (evt) => {
+      console.info('uiContainer key event:', evt.type, evt.keyCode);
+      if (evt.keyCode !== 404 && evt.keyCode !== 172) {
+        if (evt.keyCode in ARROW_KEY_CODE) {
+          navigate(ARROW_KEY_CODE[evt.keyCode]);
+        } else if (evt.keyCode === 13 || evt.keyCode === 32) {
+          // "OK" button
+          document.querySelector(':focus').click();
+        } else if (evt.keyCode === 27) {
+          // Back button
+          uiContainer.style.display = 'none';
+          uiContainer.blur();
+        }
+        evt.preventDefault();
+        evt.stopPropagation();
+      }
+    },
+    true
+  );
+
+  uiContainer.innerHTML = `
+<h1>webOS YouTube Extended</h1>
 <label for="__adblock"><input type="checkbox" id="__adblock" /> Enable AdBlocking</label>
 <label for="__sponsorblock"><input type="checkbox" id="__sponsorblock" /> Enable SponsorBlock</label>
 <blockquote>
@@ -60,140 +84,115 @@ uiContainer.innerHTML = `
 <label for="__sponsorblock_selfpromo"><input type="checkbox" id="__sponsorblock_selfpromo" /> Skip Self Promotion Segments</label>
 <label for="__sponsorblock_music_offtopic"><input type="checkbox" id="__sponsorblock_music_offtopic" /> Skip Music and Off-topic Segments</label>
 </blockquote>
-<label for="__dearrow"><input type="checkbox" id="__dearrow" /> Enable DeArrow</label>
-<blockquote>
-<label for="__dearrow_thumbnails"><input type="checkbox" id="__dearrow_thumbnails" /> Enable DeArrow Thumbnails</label>
-<div><small>DeArrow Thumbnail changing might break the shelve renderer. Be warned.</small></div>
-</blockquote>
-<label for="__barColor">Navigation Bar Color: <input type="text" id="__barColor"/></label>
-<label for="__routeColor">Main Content Color: <input type="text" id="__routeColor"/></label>
 <div><small>Sponsor segments skipping - https://sponsor.ajay.app</small></div>
 `;
+  document.querySelector('body').appendChild(uiContainer);
 
-document.querySelector('body').appendChild(uiContainer);
-
-uiContainer.querySelector('#__adblock').checked = configRead('enableAdBlock');
-uiContainer.querySelector('#__adblock').addEventListener('change', (evt) => {
-  configWrite('enableAdBlock', evt.target.checked);
-});
-
-uiContainer.querySelector('#__sponsorblock').checked =
-  configRead('enableSponsorBlock');
-uiContainer
-  .querySelector('#__sponsorblock')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlock', evt.target.checked);
+  uiContainer.querySelector('#__adblock').checked = configRead('enableAdBlock');
+  uiContainer.querySelector('#__adblock').addEventListener('change', (evt) => {
+    configWrite('enableAdBlock', evt.target.checked);
   });
 
-uiContainer.querySelector('#__sponsorblock_sponsor').checked = configRead(
-  'enableSponsorBlockSponsor'
-);
-uiContainer
-  .querySelector('#__sponsorblock_sponsor')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlockSponsor', evt.target.checked);
-  });
+  uiContainer.querySelector('#__sponsorblock').checked =
+    configRead('enableSponsorBlock');
+  uiContainer
+    .querySelector('#__sponsorblock')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlock', evt.target.checked);
+    });
 
-uiContainer.querySelector('#__sponsorblock_intro').checked = configRead(
-  'enableSponsorBlockIntro'
-);
-uiContainer
-  .querySelector('#__sponsorblock_intro')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlockIntro', evt.target.checked);
-  });
-
-uiContainer.querySelector('#__sponsorblock_outro').checked = configRead(
-  'enableSponsorBlockOutro'
-);
-uiContainer
-  .querySelector('#__sponsorblock_outro')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlockOutro', evt.target.checked);
-  });
-
-uiContainer.querySelector('#__sponsorblock_interaction').checked = configRead(
-  'enableSponsorBlockInteraction'
-);
-uiContainer
-  .querySelector('#__sponsorblock_interaction')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlockInteraction', evt.target.checked);
-  });
-
-uiContainer.querySelector('#__sponsorblock_selfpromo').checked = configRead(
-  'enableSponsorBlockSelfPromo'
-);
-uiContainer
-  .querySelector('#__sponsorblock_selfpromo')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlockSelfPromo', evt.target.checked);
-  });
-
-uiContainer.querySelector('#__sponsorblock_music_offtopic').checked =
-  configRead('enableSponsorBlockMusicOfftopic');
-uiContainer
-  .querySelector('#__sponsorblock_music_offtopic')
-  .addEventListener('change', (evt) => {
-    configWrite('enableSponsorBlockMusicOfftopic', evt.target.checked);
-  });
-
-uiContainer.querySelector('#__dearrow').checked = configRead('enableDeArrow');
-uiContainer.querySelector('#__dearrow').addEventListener('change', (evt) => {
-  configWrite('enableDeArrow', evt.target.checked);
-});
-
-uiContainer.querySelector('#__dearrow_thumbnails').checked = configRead('enableDeArrowThumbnails');
-uiContainer.querySelector('#__dearrow_thumbnails').addEventListener('change', (evt) => {
-  configWrite('enableDeArrowThumbnails', evt.target.checked);
-});
-
-uiContainer.querySelector('#__barColor').value = configRead('focusContainerColor');
-uiContainer.querySelector('#__barColor').addEventListener('change', (evt) => {
-  configWrite('focusContainerColor', evt.target.value);
-  updateStyle();
-});
-
-uiContainer.querySelector('#__routeColor').value = configRead('routeColor');
-uiContainer.querySelector('#__routeColor').addEventListener('change', (evt) => {
-  configWrite('routeColor', evt.target.value);
-  updateStyle();
-});
-
-const eventHandler = (evt) => {
-  console.info(
-    'Key event:',
-    evt.type,
-    evt.keyCode,
-    evt.keyCode,
-    evt.defaultPrevented
+  uiContainer.querySelector('#__sponsorblock_sponsor').checked = configRead(
+    'enableSponsorBlockSponsor'
   );
-  if (evt.keyCode == 404 || evt.keyCode == 172) {
-    console.info('Taking over!');
-    evt.preventDefault();
-    evt.stopPropagation();
-    if (evt.type === 'keydown') {
-      if (uiContainer.style.display === 'none') {
-        console.info('Showing and focusing!');
-        uiContainer.style.display = 'block';
-        uiContainer.focus();
-      } else {
-        console.info('Hiding!');
-        uiContainer.style.display = 'none';
-        uiContainer.blur();
-      }
-    }
-    return false;
-  }
-  return true;
-};
+  uiContainer
+    .querySelector('#__sponsorblock_sponsor')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlockSponsor', evt.target.checked);
+    });
 
-// Red, Green, Yellow, Blue
-// 403, 404, 405, 406
-// ---, 172, 170, 191
-document.addEventListener('keydown', eventHandler, true);
-document.addEventListener('keypress', eventHandler, true);
-document.addEventListener('keyup', eventHandler, true);
+  uiContainer.querySelector('#__sponsorblock_intro').checked = configRead(
+    'enableSponsorBlockIntro'
+  );
+  uiContainer
+    .querySelector('#__sponsorblock_intro')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlockIntro', evt.target.checked);
+    });
+
+  uiContainer.querySelector('#__sponsorblock_outro').checked = configRead(
+    'enableSponsorBlockOutro'
+  );
+  uiContainer
+    .querySelector('#__sponsorblock_outro')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlockOutro', evt.target.checked);
+    });
+
+  uiContainer.querySelector('#__sponsorblock_interaction').checked = configRead(
+    'enableSponsorBlockInteraction'
+  );
+  uiContainer
+    .querySelector('#__sponsorblock_interaction')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlockInteraction', evt.target.checked);
+    });
+
+  uiContainer.querySelector('#__sponsorblock_selfpromo').checked = configRead(
+    'enableSponsorBlockSelfPromo'
+  );
+  uiContainer
+    .querySelector('#__sponsorblock_selfpromo')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlockSelfPromo', evt.target.checked);
+    });
+
+  uiContainer.querySelector('#__sponsorblock_music_offtopic').checked =
+    configRead('enableSponsorBlockMusicOfftopic');
+  uiContainer
+    .querySelector('#__sponsorblock_music_offtopic')
+    .addEventListener('change', (evt) => {
+      configWrite('enableSponsorBlockMusicOfftopic', evt.target.checked);
+    });
+  var eventHandler = (evt) => {
+    // We handle key events ourselves.
+    console.info(
+      'Key event:',
+      evt.type,
+      evt.keyCode,
+      evt.keyCode,
+      evt.defaultPrevented
+    );
+    if (evt.keyCode == 404 || evt.keyCode == 172) {
+      console.info('Taking over!');
+      evt.preventDefault();
+      evt.stopPropagation();
+      if (evt.type === 'keydown') {
+        if (uiContainer.style.display === 'none') {
+          console.info('Showing and focusing!');
+          uiContainer.style.display = 'block';
+          uiContainer.focus();
+        } else {
+          console.info('Hiding!');
+          uiContainer.style.display = 'none';
+          uiContainer.blur();
+        }
+      }
+      return false;
+    }
+    return true;
+  };
+
+  // Red, Green, Yellow, Blue
+  // 403, 404, 405, 406
+  // ---, 172, 170, 191
+  document.addEventListener('keydown', eventHandler, true);
+  document.addEventListener('keypress', eventHandler, true);
+  document.addEventListener('keyup', eventHandler, true);
+
+  setTimeout(() => {
+    showNotification('Press [GREEN] to open YTAF configuration screen\nPress [BLUE] to open Video Speed configuration screen');
+  }, 2000);
+}
 
 export function showNotification(text, time = 3000) {
   if (!document.querySelector('.ytaf-notification-container')) {
@@ -221,7 +220,3 @@ export function showNotification(text, time = 3000) {
     }, 1000);
   }, time);
 }
-
-setTimeout(() => {
-  showNotification('Press [GREEN] to open TizenTube configuration screen\nPress [BLUE] to open Video Speed configuration screen');
-}, 2000);
