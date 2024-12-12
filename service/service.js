9bbde6ba7a7b9b2e17b2729c0de9319354be7678
reis <29177546+reisxd@users.noreply.github.com>
2024-12-12 21:30:51 +0300

    feat: casting while in app

--- service/service.js
+++ service/service.js
@@ -1,6 +1,17 @@
 const dial = require("@patrickkfkan/peer-dial");
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
 const PORT = 8085;
 const apps = {
     "YouTube": {
@@ -8,6 +19,7 @@ const apps = {
         state: "stopped",
         allowStop: true,
         pid: null,
+        additionalData: {},
         launch(launchData) {
             const tbPackageId = tizen.application.getAppInfo().packageId;
             tizen.application.launchAppControl(
@@ -45,6 +57,26 @@ const dialServer = new dial.Server({
             console.log(`Got request to launch ${appName} with launch data: ${launchData}`);
             const app = apps[appName];
             if (app) {
+                const parsedData = launchData.split('&').reduce((acc, cur) => {
+                    const parts = cur.split('=');
+                    const key = parts[0];
+                    const value = parts[1];
+                
+                    if (typeof value !== 'undefined') {
+                        acc[key] = value;
+                    } else {
+                        acc[key] = '';
+                    }
+                
+                    return acc;
+                }, {});
+                
+                if (parsedData.yumi) {
+                    app.additionalData = parsedData;
+                    app.state = "running"
+                    callback("");
+                    return;
+                }
                 app.pid = "run";
                 app.state = "starting";
                 app.launch(launchData);
@@ -65,6 +97,19 @@ const dialServer = new dial.Server({
         }
     }
 });
+
+
+setInterval(() => {
+    tizen.application.getAppsContext((appsContext) => {
+        const tbPackageId = tizen.application.getAppInfo().packageId;
+        const app = appsContext.find(app => app.appId === `${tbPackageId}.TizenBrewStandalone`);
+        if (!app) {
+            apps["YouTube"].state = "stopped";
+            apps["YouTube"].pid = null;
+            apps["YouTube"].additionalData = {};
+        }
+    });
+}, 5000);
 
 app.listen(PORT, () => {
     dialServer.start();
