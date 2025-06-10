40b4666a22d4372f25b6ae424a4528560791a29c
reis <29177546+reisxd@users.noreply.github.com>
2025-06-10 21:53:57 +0300

    fix: don't change every thumbnail for hq

--- mods/adblock.js
+++ mods/adblock.js
@@ -192,20 +192,17 @@ function deArrowify(items) {
 function hqify(items) {
   for (const item of items) {
     if (item.tileRenderer.style !== 'TILE_STYLE_YTLR_DEFAULT') continue;
-    let quality = 'sddefault';
     if (configRead('enableHqThumbnails')) {
-      quality = 'maxresdefault';
+      const videoID = item.tileRenderer.contentId;
+      const queryArgs = item.tileRenderer.header.tileHeaderRenderer.thumbnail.thumbnails[0].url.split('?')[1];
+      item.tileRenderer.header.tileHeaderRenderer.thumbnail.thumbnails = [
+        {
+          url: `https://i.ytimg.com/vi/${videoID}/sddefault.jpg${queryArgs ? `?${queryArgs}` : ''}`,
+          width: 640,
+          height: 480
+        }
+      ];
     }
-
-    const videoID = item.tileRenderer.contentId;
-    const queryArgs = item.tileRenderer.header.tileHeaderRenderer.thumbnail.thumbnails[0].url.split('?')[1];
-    item.tileRenderer.header.tileHeaderRenderer.thumbnail.thumbnails = [
-      {
-        url: `https://i.ytimg.com/vi/${videoID}/${quality}.jpg${queryArgs ? `?${queryArgs}` : ''}`,
-        width: quality === 'maxresdefault' ? 1280 : 640,
-        height: quality === 'maxresdefault' ? 720 : 480
-      }
-    ];
   }
 }
 
