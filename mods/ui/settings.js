43468795c83e9e4a2bc1fdbabf241bbd9e40b7ee
KrX3D <KrX3D@users.noreply.github.com>
2026-03-04 02:35:33 +0100

    fix

--- mods/ui/settings.js
+++ mods/ui/settings.js
@@ -566,22 +566,6 @@ export default function modernUI(update, parameters) {
                                     value: 'watch'
                                 }
                             ]
-                        },
-                        {
-                            name: 'Choose Library Tabs / Buttons to Hide',
-                            icon: 'TAB_LIBRARY',
-                            value: null,
-                            arrayToEdit: 'hiddenLibraryTabIds',
-                            menuId: 'tt-hidden-library-tabs',
-                            options: [
-                                { name: 'Music', value: 'femusic_last_played' },
-                                { name: 'Movies & Shows', value: 'festorefront' },
-                                { name: 'Podcasts', value: 'fecollection_podcasts' },
-                                { name: 'My Videos', value: 'femy_videos' },
-                                { name: 'History', value: 'fehistory' },
-                                { name: 'Watch Later', value: 'femy_youtube' },
-                                { name: 'Playlists', value: 'feplaylist_aggregation' }
-                            ]
                         }
                     ]
                 },
