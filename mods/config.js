411885e35c4d92dedcf5fcd72f2926220b45f17f
Reis Can <29177546+reisxd@users.noreply.github.com>
2026-09-11 18:19:04 +0300

    fix: build not finishing

--- mods/config.js
+++ mods/config.js
@@ -19,7 +19,7 @@ const defaultConfig = {
   enableDeArrowThumbnails: false,
   focusContainerColor: '#0f0f0f',
   routeColor: '#0f0f0f',
-  enableFixedUI: (window.h5vcc && window.h5vcc.tizentube) ? false : true,
+  enableFixedUI: (window.h5vcc) ? false : true,
   enableHqThumbnails: false,
   enableChapters: true,
   enableLongPress: true,
